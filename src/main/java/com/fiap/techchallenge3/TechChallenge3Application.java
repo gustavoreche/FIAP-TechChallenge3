16ace126ba0f69a431b01bc02c3fdddd31d54d3b
Gustavo Reche <gustavodreche@msn.com>
2024-03-07 15:39:40 -0300

    Cria projeto com dependencias

---
+++ src/main/java/com/fiap/techchallenge3/TechChallenge3Application.java
@@ -0,0 +1,13 @@
+package com.fiap.techchallenge3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechChallenge3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechChallenge3Application.class, args);
+	}
+
+}
