a5921600fd3588edd0e531c3565ba82a5e79358a
Gustavo Reche <gustavodreche@msn.com>
2024-04-22 22:49:35 -0300

    Cria reserva de mesa, sem teste

---
+++ src/main/java/com/fiap/techchallenge3/model/Reserva.java
@@ -0,0 +1,32 @@
+package com.fiap.techchallenge3.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "tb_reserva")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Reserva {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String cnpjRestaurante;
+    private String cpfCnpjCliente;
+    @Enumerated(EnumType.STRING)
+    private DiasEnum dia;
+    private String horarioDeChegada;
+    private int quantidadeLugaresClienteDeseja;
+    private LocalDateTime horarioDaReservaRealizada;
+    @Enumerated(EnumType.STRING)
+    private StatusReservaEnum statusReserva;
+
+}
---
+++ src/main/java/com/fiap/techchallenge3/repository/ReservaRepository.java
@@ -0,0 +1,8 @@
+package com.fiap.techchallenge3.repository;
+
+import com.fiap.techchallenge3.model.Reserva;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReservaRepository extends JpaRepository<Reserva, Long> {
+
+}
